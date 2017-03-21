6643d54a543b2dcd51ce0230e34d35abfbdf977c
zhangln <610857416@qq.com>
2017-03-21 15:34:24 +0800

    添加hello world

--- src/com/test/idea/HelloWorld.java
+++ src/com/test/idea/HelloWorld.java
@@ -4,5 +4,7 @@ package com.test.idea;
  * Created by admin on 2017/3/21.
  */
 public class HelloWorld {
-
+    public static void main(String[] args) {
+        System.out.print("hello world");
+    }
 }
